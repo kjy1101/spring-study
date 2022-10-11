b25b26c04241f0314b3ea401ca14eebf7b104c8b
Jaeyeon Kim <amelia1023@naver.com>
2022-10-11 15:54:36 +0900

    return view (controller)

--- src/main/java/service/GreetingService.java
+++ src/main/java/service/GreetingService.java
@@ -1,10 +1,7 @@
 package service;
 
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class GreetingService {
     public static String greeting() {
-        return "<h1>핫도그 세개 주세요</h1>";
+        return "greeting";
     }
 }
--- src/main/java/web/GreetingController.java
+++ src/main/java/web/GreetingController.java
@@ -1,11 +1,11 @@
 package web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import service.GreetingService;
 
-@RestController
+@Controller
 public class GreetingController {
     String result;
 
