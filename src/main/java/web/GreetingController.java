e020cb0a297cc3e30a7c5b87fb834cd0500feb0a
Jaeyeon Kim <amelia1023@naver.com>
2022-10-13 13:24:15 +0900

    memberRepository (+ test code)

--- src/main/java/web/GreetingController.java
+++ src/main/java/web/GreetingController.java
@@ -3,6 +3,8 @@ package web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import service.GreetingService;
 
@@ -20,5 +22,38 @@ public class GreetingController {
     public String getMessage(Model model){
         model.addAttribute("testSTR", "왜 타임리프라고 읽나요");
         return "testView";
+    }
+
+    @GetMapping("/param")
+    public String paramTest(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "param";
+    }
+
+    @GetMapping("/string")
+    @ResponseBody
+    public String stringTest(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello " + name;
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    
+    static class Hello {
+        private String name; // 외부에서 접근 x => 라이브러리나 아래 메소드를 통해 접근
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
