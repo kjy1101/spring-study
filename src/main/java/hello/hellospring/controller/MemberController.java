a3eb2af0474bf6023129dcdd8e49b1a220841740
Jaeyeon Kim <amelia1023@naver.com>
2022-10-17 14:47:49 +0900

    controller annotation add

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,11 +1,11 @@
 package hello.hellospring.controller;
 
 // import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 
 import hello.hellospring.service.MemberService;
 
-// @Controller
+@Controller
 public class MemberController {
     private final MemberService memberService;
 
