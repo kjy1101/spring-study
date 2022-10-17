0d85b5a9ba3b35390ed614b03c6082c0e24a2073
Jaeyeon Kim <amelia1023@naver.com>
2022-10-17 16:21:09 +0900

    member register&list mvc

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"web"})
+// @ComponentScan(basePackages = {"controller"})
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
--- src/main/java/service/GreetingService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-public class GreetingService {
-    public static String greeting() {
-        return "greeting";
-    }
-}
